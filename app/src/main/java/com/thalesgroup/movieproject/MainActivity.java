fdd1a8d8cad96d71adb4552202968af5ed48b09f
nightingale <nightingale@google.com>
2020-09-26 01:45:40 -0400

    add simple gui

--- app/src/main/java/com/thalesgroup/movieproject/MainActivity.java
+++ app/src/main/java/com/thalesgroup/movieproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.thalesgroup.movieproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * call TMDB rest-api
+     *
+     * @param view the view
+     */
+    public void onButtonSearchClick(View view) {
+        
     }
 }
